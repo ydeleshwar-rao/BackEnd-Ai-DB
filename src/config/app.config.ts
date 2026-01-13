621a926816eebbdcc984d27d9edad042c040cd0a
Y-Deleshwar <y.deleshwar.rao@nirvanagroup.co.in>
2026-01-14 00:04:32 +0530

    fix COR issue

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -5,10 +5,28 @@ import { errorHandler } from '../utils/errorHandler';
 export function createApp() {
   const app = express();
 
-  app.use(cors({
-    origin: 'https://front-end-ai-db.vercel.app/',
-    credentials: true
-  }));
+  const allowedOrigins = [
+    'https://front-end-ai-db.vercel.app',
+    'http://localhost:5173',
+    'http://localhost:4000'
+  ];
+
+  app.use(
+    cors({
+      origin: function (origin, callback) {
+        
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      credentials: true
+    })
+  );
+
   app.use(express.json());
 
   return app;
